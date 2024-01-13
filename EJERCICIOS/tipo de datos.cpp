c3e81de4ee8bcbc5752a0b479674a14fb573a55f
xtramer123 <150769609+xtramer123@users.noreply.github.com>
2024-01-13 12:05:52 -0500

    first commit

---
+++ EJERCICIOS/tipo de datos.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    // entero
+    short n;
+    int n2;
+    // flotantes
+    float y;
+    double y2;
+    // prefijos
+    // texto
+    char v = 'a';
+    string v2 = "carlos";
+    // bolean
+    bool controlador = false;
+}
---
+++ EJERCICIOS/tutorial.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int a, b;
+    cout << "Ingresa numero 1: ";
+    cin >> a;
+    cout << "Ingresa el numero 2: ";
+    cin >> b;
+    cout << "Suma: " << a + b;
+}
